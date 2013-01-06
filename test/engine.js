724765bd11efe90b893a1a4ce82f94646cf9b176
ajlopez <webmaster@ajlopez.com>
2013-01-06 17:44:23 -0300

    Engine createDimension, getDimensions, tests

--- test/engine.js
+++ test/engine.js
@@ -12,5 +12,22 @@ assert.equal(typeof simplememolap.createEngine, 'function');
 var engine = simplememolap.createEngine();
 assert.ok(engine);
 
+// Create dimensions
 
+var dimcountry = engine.createDimension('country');
+assert.ok(dimcountry);
+assert.equal(dimcountry.name, 'country');
 
+var dimcategory = engine.createDimension('category');
+assert.ok(dimcategory);
+assert.equal(dimcategory.name, 'category');
+
+var dimproduct = engine.createDimension('product');
+assert.ok(dimproduct);
+assert.equal(dimproduct.name, 'product');
+
+var dimensions = engine.getDimensions();
+assert.ok(dimensions);
+assert.ok(dimensions.country);
+assert.ok(dimensions.category);
+assert.ok(dimensions.product);
