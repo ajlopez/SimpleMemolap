c5884fe4fd7b4064f21da6c091237aff31f11fdd
ajlopez <webmaster@ajlopez.com>
2015-12-27 15:51:57 -0300

    Add values first test

--- test/stores.js
+++ test/stores.js
@@ -1,6 +1,8 @@
 
 var stores = require('../lib/stores');
 
+var store;
+
 exports['create store'] = function (test) {
     var store = stores.store({ dimensions: 3 });
     
@@ -18,4 +20,12 @@ exports['create store without dimensions raise an exception'] = function (test) 
         "Number of dimensions is required to create a store"
     );
 };
+
+exports['add values and data'] = function (test) {
+    store = stores.store({ dimensions: 3 });
+    store.add([1, 2, 3], "foo");
+    
+    test.equal(store.size(), 1);
+};
+
 
