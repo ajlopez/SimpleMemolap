69fc107a9cbb630b2d42db20dad0dfe4342c8fd0
ajlopez <webmaster@ajlopez.com>
2015-12-27 15:39:39 -0300

    Create store without dimensions

--- test/stores.js
+++ test/stores.js
@@ -10,3 +10,12 @@ exports['create store'] = function (test) {
     test.equal(store.size(), 0);
 };
 
+exports['create store without dimensions raise an exception'] = function (test) {
+    test.throws(
+        function () {
+            stores.store();
+        },
+        "Number of dimensions is required to create a store"
+    );
+};
+
